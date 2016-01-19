bec931fc63d4aa4a9e3eb9590b3b5281c9fd9efb
Eric Foertsch <efoertsch@fisincorporated.com>
2016-01-19 15:34:53 -0500

    Fixed some navigation and menu issues. Put FAB on Statistics to display map rather than via menu.

--- exerciseTracker/src/main/java/com/fisincorporated/ExerciseTracker/FunctionListActivity.java
+++ exerciseTracker/src/main/java/com/fisincorporated/ExerciseTracker/FunctionListActivity.java
@@ -1,10 +1,16 @@
 package com.fisincorporated.ExerciseTracker;
 
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class FunctionListActivity extends ExerciseMasterFragmentActivity {
 
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		actionBar.setDisplayHomeAsUpEnabled(false);
+	}
 
 	@Override
 	protected Fragment createFragment() {
